13260887d3d4472655f35a256f8e38d28c1e94fa
ٱللَّٰه‎➳𝘼Ᏸ𝔇ᴜ͢ŁŁΛH҉ <161955932+Xylon-404@users.noreply.github.com>
2025-06-10 08:31:09 +0600

    Update install.py

--- install.py
+++ install.py
@@ -3,4 +3,5 @@ import os
 os.system("pip install requests")
 os.system("pip install colorama")
 os.system("pip install rich")
+os.system("pip install prompt_toolkit")
 os.system("python sms-404.py")
